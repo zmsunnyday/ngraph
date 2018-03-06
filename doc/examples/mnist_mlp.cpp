ea390a62d86e0690a55f77bb0838b08abe439d05
Scott Cyphers <scott.cyphers@intel.com>
2018-03-05 18:57:54 -0800

    Through cost

--- doc/examples/mnist_mlp.cpp
+++ doc/examples/mnist_mlp.cpp
@@ -15,25 +15,77 @@
 *******************************************************************************/
 
 #include <cstdio>
+#include <iostream>
 #include <memory>
 #include <string>
 
 #include <ngraph/ngraph.hpp>
 
-#include "layers.hpp"
 #include "mnist.hpp"
 
 using namespace ngraph;
 
+std::ostream& operator<<(std::ostream& s, const Shape& shape)
+{
+    s << '(';
+    for (auto l : shape)
+    {
+        s << l << ", ";
+    }
+    s << ")";
+    return s;
+}
+
 int main(int argc, const char* argv[])
 {
     size_t batch_size = 128;
+    size_t hidden_size = 500;
+    size_t output_size = 10;
+    float log_min = -50.0f;
     MNistDataLoader test_loader{batch_size, MNistImageLoader::TEST, MNistLabelLoader::TEST};
-    size_t input_size = test_loader.get_columns() * test_loader.get_rows();
     test_loader.open();
+    size_t input_size = test_loader.get_columns() * test_loader.get_rows();
 
-    auto x_input = std::make_shared<InputLayer>("X", element::f32, Shape{batch_size, input_size});
-    auto mlp_0 = std::make_shared<MLPLayer>("MLP_0", x_input, 10);
+    // The data input
+    auto X = std::make_shared<op::Parameter>(element::f32, Shape{batch_size, input_size});
+    auto Y = std::make_shared<op::Parameter>(element::f32, Shape{batch_size});
+
+    // Layer 0
+    auto W0 = std::make_shared<op::Parameter>(element::f32, Shape{input_size, hidden_size});
+    auto b0 = std::make_shared<op::Parameter>(element::f32, Shape{hidden_size});
+    auto l0_dot = std::make_shared<op::Dot>(X, W0, 1);
+    auto b0_broadcast =
+        std::make_shared<op::Broadcast>(b0, Shape{batch_size, hidden_size}, AxisSet{0});
+    auto l0_sum = std::make_shared<op::Add>(l0_dot, b0_broadcast);
+    auto l0 = std::make_shared<op::Tanh>(l0_sum);
+
+    // Layer 1
+    auto W1 = std::make_shared<op::Parameter>(element::f32, Shape{hidden_size, output_size});
+    auto b1 = std::make_shared<op::Parameter>(element::f32, Shape{output_size});
+    auto l1_dot = std::make_shared<op::Dot>(l0, W1, 1);
+    auto b1_broadcast =
+        std::make_shared<op::Broadcast>(b1, Shape{batch_size, output_size}, AxisSet{0});
+    auto l1_sum = std::make_shared<op::Add>(l1_dot, b1_broadcast);
+    auto l1 = std::make_shared<op::Tanh>(l1_sum);
+
+    // Softmax
+    auto sm = std::make_shared<op::Softmax>(l1, AxisSet{1});
+    std::cout << sm->get_output_shape(0) << std::endl;
+
+    // Cost computation
+    auto labels = std::make_shared<op::OneHot>(Y, Shape{batch_size, output_size}, 1);
+    auto sm_clip_value =
+        std::make_shared<op::Constant>(element::f32, Shape{}, std::vector<float>{log_min});
+    auto sm_clip_broadcast = std::make_shared<op::Broadcast>(
+        sm_clip_value, Shape{batch_size, output_size}, AxisSet{0, 1});
+    auto sm_clip = std::make_shared<op::Maximum>(sm, sm_clip_broadcast);
+    auto sm_log = std::make_shared<op::Log>(sm_clip);
+    std::cout << labels->get_output_shape(0) << std::endl;
+    auto prod = std::make_shared<op::Multiply>(sm_clip, labels);
+    auto cross_entropy = std::make_shared<op::Sum>(prod, AxisSet{0, 1});
+
+    // Backprop
+    
 
     return 0;
 }
